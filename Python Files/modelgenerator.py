08759ef949b47d6af55dd50596b0bd194fd370ec
ashleshmahule <ashleshmahule15@gmail.com>
2020-03-25 00:25:47 +0530

    Changes in python file

--- Python Files/modelgenerator.py
+++ Python Files/modelgenerator.py
@@ -43,23 +43,23 @@ validation_generator = validation_datagen.flow_from_directory(
 model = tf.keras.models.Sequential([
     # Note the input shape is the desired size of the image 150x150 with 3 bytes color
     # This is the first convolution
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(150, 150, 3)),
     tf.keras.layers.MaxPooling2D(2, 2),
     # The second convolution
-    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2, 2),
     # The third convolution
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2, 2),
     # The fourth convolution
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2, 2),
     # Flatten the results to feed into a DNN
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dropout(0.5),
     # 512 neuron hidden layer
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(27, activation='softmax')
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(28, activation='softmax')
 ])
 
 print(model.summary())
