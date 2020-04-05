9cca19a6fc0d9b1175997eab0fb16e380318a25b
Ashlesh Mahule <ashleshmahule15@gmail.com>
2020-04-05 22:21:46 +0530

    Update DescriptionActivity.java

--- Android App (Android Studio Project)/VTGSTech20/app/src/main/java/com/example/vtg_stech20/DescriptionActivity.java
+++ Android App (Android Studio Project)/VTGSTech20/app/src/main/java/com/example/vtg_stech20/DescriptionActivity.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -24,12 +24,14 @@ import java.util.Map;
 public class DescriptionActivity extends AppCompatActivity {
 
     Map<String, Object> result = new HashMap<>();
-    TextView nametv, loctv, statetv, desctv;
+    TextView nametv, loctv, statetv, desctv, likes;
     EditText userReview;
     List<String> re;
     ListView reviews;
     String place;
-    Button add, openMap;
+    Button add, openMap, heart;
+    long nlikes = 0;
+    boolean isliked=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +59,7 @@ public class DescriptionActivity extends AppCompatActivity {
                     place = result.get("Place").toString();
                     String state = result.get("State").toString();
                     String desc = result.get("Description").toString();
+                    nlikes = (Long) result.get("Likes");
 
                     nametv = findViewById(R.id.name);
                     nametv.setText(name);
@@ -70,6 +73,9 @@ public class DescriptionActivity extends AppCompatActivity {
 
                     desctv = findViewById(R.id.description);
                     desctv.setText(desc);
+
+                    likes = findViewById(R.id.likes);
+                    likes.setText("" + nlikes);
 
                     re = (List<String>) result.get("Reviews");
 
@@ -86,6 +92,23 @@ public class DescriptionActivity extends AppCompatActivity {
             }
         });
 
+        heart = findViewById(R.id.heart);
+        heart.setOnClickListener(v -> {
+            isliked=true;
+            Map<String,Object> map=new HashMap<>();
+            map.put("Likes",nlikes+1);
+            Toast.makeText(this, "You liked this!", Toast.LENGTH_SHORT).show();
+            dr.set(map, SetOptions.merge()).addOnSuccessListener(aVoid -> Log.println(7, "add review", "DocumentSnapshot successfully written!")).addOnFailureListener(e -> Log.println(7, "add likes", "Error writing document " + e));
+            finish();
+            overridePendingTransition(0, 0);
+            startActivity(getIntent());
+            overridePendingTransition(0, 0);
+        });
+
+        if(isliked) {
+            heart.setBackground(getDrawable(R.drawable.hearten));
+        }
+
         add = findViewById(R.id.add);
         add.setOnClickListener(v -> {
             userReview = findViewById(R.id.userReview);
@@ -93,6 +116,7 @@ public class DescriptionActivity extends AppCompatActivity {
             re.add(ur);
             Map<String, Object> map = new HashMap<>();
             map.put("Reviews", re);
+            Toast.makeText(this,"Review succesfully added!",Toast.LENGTH_SHORT).show();
 
             dr.set(map, SetOptions.merge()).addOnSuccessListener(aVoid -> Log.println(7, "add review", "DocumentSnapshot successfully written!")).addOnFailureListener(e -> Log.println(7, "add review", "Error writing document " + e));
             finish();
